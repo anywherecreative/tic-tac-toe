7ded14f756b601ec1dff8274eb09049816fb1982
Jeff Manning <jeff@anywherecreative.ca>
2018-06-12 17:08:07 -0400

    win conditions added

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
       return; //can't reuse a square
     }
     $(this).addClass('used');
-    $(this).data('player',currentPlayer);
+    $(this).addClass(currentPlayer);
     $(this).text(currentPlayer);
     if(currentPlayer == "O") {
       currentPlayer = "X"
@@ -13,5 +13,64 @@ $(document).ready(function() {
     else {
       currentPlayer = "O"
     }
+    checkWin();
   })
 });
+
+function checkWin() {
+  //check horizontal
+  $('.row').each(function() {
+    if($(".X",this).length == 3) {
+      alert('X wins!');
+    }
+    if($(".O",this).length == 3) {
+      alert('O wins!');
+    }
+  })
+
+  //check vertical
+  for(var a = 1;a <= 3;a++) {
+    if($(".row:nth-child(1) .square:nth-child(" + a + ")").hasClass('O') &&
+       $(".row:nth-child(2) .square:nth-child(" + a + ")").hasClass('O') &&
+       $(".row:nth-child(3) .square:nth-child(" + a + ")").hasClass('O')
+     ) {
+       alert('O wins!');
+       break;
+     }
+
+     if($(".row:nth-child(1) .square:nth-child(" + a + ")").hasClass('X') &&
+        $(".row:nth-child(2) .square:nth-child(" + a + ")").hasClass('X') &&
+        $(".row:nth-child(3) .square:nth-child(" + a + ")").hasClass('X')
+      ) {
+        alert('X wins!');break;
+      }
+    }
+
+    //check diagonal
+    if($(".row:nth-child(1) .square:nth-child(1)").hasClass('O') &&
+      $(".row:nth-child(2) .square:nth-child(2)").hasClass('O') &&
+      $(".row:nth-child(3) .square:nth-child(3)").hasClass('O')
+      ) {
+        alert('O wins!');
+    }
+    if($(".row:nth-child(3) .square:nth-child(1)").hasClass('O') &&
+      $(".row:nth-child(2) .square:nth-child(2)").hasClass('O') &&
+      $(".row:nth-child(1) .square:nth-child(3)").hasClass('O')
+      ) {
+        alert('O wins!');
+    }
+
+    //check diagonal
+    if($(".row:nth-child(1) .square:nth-child(1)").hasClass('X') &&
+      $(".row:nth-child(2) .square:nth-child(2)").hasClass('X') &&
+      $(".row:nth-child(3) .square:nth-child(3)").hasClass('X')
+      ) {
+        alert('O wins!');
+    }
+    if($(".row:nth-child(3) .square:nth-child(1)").hasClass('X') &&
+      $(".row:nth-child(2) .square:nth-child(2)").hasClass('X') &&
+      $(".row:nth-child(1) .square:nth-child(3)").hasClass('X')
+      ) {
+        alert('O wins!');
+    }
+  }
